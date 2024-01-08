4725a2d522f8ebaac6c21f453d521513043c0f88
Dieter Whittingham <dieter.whitt@gmail.com>
2024-01-07 22:10:07 -0500

    ok

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useState, useEffect } from 'react';
+
+//routing
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Intervals from "./pages/Intervals";
+import Chords from "./pages/Chords";
+import Cadences from "./pages/Cadences"
+import About from "./pages/About"
+import NoPage from "./pages/NoPage";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
