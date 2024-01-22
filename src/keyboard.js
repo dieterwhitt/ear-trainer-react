909d75d7a73011c12a53a83cb42ad4e024ff6671
Dieter Whittingham <dieter.whitt@gmail.com>
2024-01-22 15:05:32 -0500

    working on triads and reworked the bass note selection

--- src/keyboard.js
+++ src/keyboard.js
@@ -279,6 +279,95 @@ function numeralToScaleIndex(numeral){
 }
 
 /**
+ * 
+ * @param {*} scale scale object
+ * @param {*} low keyboard index of the lower bound to search
+ * @param {*} high index of the upper bound
+ * @returns - keyboard index of the searched note, or false if no note is found
+ */
+function scaleToNote(scale, low, high){
+    //find the first tonic note of a scale in a given range
+    for(let index = low; index <=high; index ++){
+        //check if the scale note matches the current keyboard note
+        if(scale[0].slice(0, -1) === keyboard[index].slice(1)){
+            return index;
+        }
+    }
+    return false;
+}
+/**
+ * compartmentalizing the selection of the bass note
+ * splits into 2 candidates then returns the best one
+ * @param {int} previousNote - the previous note on the keyboard
+ * @param {int} noteDiff - the distanc e(semitones) from the previous scale position to the 
+ * current
+ * @param {boolean} cadential - boolean representing if the current chord is a Cad 6/4
+ * @param {int} tonic - tonic note of the scale for detemrining Cadential jumps
+ */
+function chooseBass(previousNote, noteDiff, cadential, tonic){
+    let cand1, cand2;
+    if (noteDiff < 0){
+        //moving down the scale
+        cand1 = previousNote + noteDiff;
+        //shift an octave up
+        cand2 = cand1 + 12;
+    }else if (noteDiff > 0){
+        //moving up the scale
+        cand2 = previousNote + noteDiff;
+        //shift an octave down
+        cand1 = cand2 - 12;
+    }else{
+        //same scale note (C -> V)
+        cand2 = previousNote + 12;
+        cand1 = previousNote - 12;
+    }
+    //now have 2 candidates
+    let candidates = [cand1, cand2];
+    console.log('current candidates:' + candidates + 
+    ' cadential: ' + cadential);
+    let chosen = -1;
+    for (const index in candidates) {
+        //remove any jump bigger than a 5th
+        if (!cadential && Math.abs(previousNote - candidates[index]) > 7){
+            //interval from previous to current is bigger than a 5th
+            //not including cadential (octave jump)
+            //remove candidate
+            candidates.splice(index, 1);
+            //choose the other object
+            chosen = candidates[0];
+        }
+    }
+    //if there are still 2 candidates
+    if (chosen === -1){
+        //handle cadential 
+        if(cadential){
+            if (previousNote < tonic){
+                //previous note was lower than the tonic
+                //jump to cand2
+                chosen = cand2;
+            }else{
+                //previous note was higher
+                //jump to cand1 (remove cand2)
+                chosen = cand1;
+            }
+        }else if (previousNote > 33){
+            //not candential and previous note was too high
+            chosen = cand1;
+        }else if (previousNote < 22){
+            //too low
+            chosen = cand2;
+        }else{
+            //choose at random
+            console.log('choosing randomly between ' + candidates);
+            const choice = Math.floor(Math.random() * 2);
+            chosen = candidates.at(choice);
+        }
+    }
+    console.log('chosen for chord: ' + chosen);
+    return chosen;
+}
+
+/**
  * creates a base line in terms of keyboard indexes
  * @param {Array} scale 
  * @param {Array} numeralProgression
@@ -288,21 +377,16 @@ function createBassProgression(scale, numeralProgression){
     //bass: getting tonic bass note
     //tonic note is from 2g-3fs which is index 22-33
     let bassProgression = [];
-    for(let index = 22; index <=33; index ++){
-        //check if the scale note matches the current keyboard note
-        if(scale[0].slice(0, -1) === keyboard[index].slice(1)){
-            bassProgression.push(index);
-        }
-    }
-    
+    const tonic = scaleToNote(scale, 22, 33);
     //other 4 notes
     //keeping track of the previous note played for harmony reasons
-    let tonic = bassProgression[0]
+    bassProgression.push(tonic);
     let previousNote = tonic;
     //keeping track of previous scale index
     let previousScaleIndex = 1;
-    let currentScaleIndex, noteDiff, cand1, cand2;
-    let cadential;
+    let currentScaleIndex, noteDiff; //integers
+    let cadential; //boolean
+    let chosen; //each chord's chosen bass note 
     //cand2 > cand1
     for (let index = 1; index <= 4; index ++) {
         //get what scale note we should be on
@@ -310,68 +394,10 @@ function createBassProgression(scale, numeralProgression){
         //difference from the previous note to the current one
         noteDiff = scale[currentScaleIndex] - scale[previousScaleIndex];
         //get the note candidates
-        //set cadential back to false
-        cadential = false;
-        if (noteDiff < 0){
-            //moving down the scale
-            cand1 = previousNote + noteDiff;
-            //shift an octave up
-            cand2 = cand1 + 12;
-        }else if (noteDiff > 0){
-            //moving up the scale
-            cand2 = previousNote + noteDiff;
-            //shift an octave down
-            cand1 = cand2 - 12;
-        }else{
-            //same scale note (C -> V)
-            cand2 = previousNote + 12;
-            cand1 = previousNote - 12;
-            //mark cadential
-            cadential = true;
-        }
-        //now have 2 candidates
-        let candidates = [cand1, cand2];
-        console.log('current candidates for chord ' + index + ': ' + candidates + 
-        ' cadential: ' + cadential);
-        let chosen = -1;
-        for (const index in candidates) {
-            //remove any jump bigger than a 5th
-            if (!cadential && Math.abs(previousNote - candidates[index]) > 7){
-                //interval from previous to current is bigger than a 5th
-                //not including cadential (octave jump)
-                //remove candidate
-                candidates.splice(index, 1);
-                //choose the other object
-                chosen = candidates[0];
-            }
-        }
-        //if there are still 2 candidates
-        if (chosen === -1){
-            //handle cadential 
-            if(cadential){
-                if (previousNote < tonic){
-                    //previous note was lower than the tonic
-                    //jump to cand2
-                    chosen = cand2;
-                }else{
-                    //previous note was higher
-                    //jump to cand1 (remove cand2)
-                    chosen = cand1;
-                }
-            }else if (previousNote > 33){
-                //not candential and previous note was too high
-                chosen = cand1;
-            }else if (previousNote < 22){
-                //too low
-                chosen = cand2;
-            }else{
-                //choose at random
-                console.log('choosing randomly between ' + candidates);
-                const choice = Math.floor(Math.random() * 2);
-                chosen = candidates.at(choice);
-            }
-        }
-        console.log('chosen for chord ' + index + ': ' + chosen);
+        //update cadential
+        numeralProgression[index] === 'C' ? cadential = false : cadential = true;
+        //calling separate function
+        chosen = chooseBass(previousNote, noteDiff, cadential, tonic);
         bassProgression.push(chosen);
         //update
         previousNote = chosen;
@@ -379,7 +405,9 @@ function createBassProgression(scale, numeralProgression){
     }
     return bassProgression;
 }
-
+/**
+ * bass line testing function
+ */
 export function testBass(){
     const cmajorScale = createScale('d', false);
     const numeralProgression = createNumeralProgression(cmajorScale);
@@ -390,3 +418,40 @@ export function testBass(){
         delay += 2000;
     }
 }
+/*
+function scaleTonicTriad(scale){
+    //1st inversion triad
+    //root: 3rd of the chord. root: 3g to 4fs
+    const root = scaleToNote(scale, 34, 45);
+    //first inversion
+    const tonicTriad = [root + scale[3], root + scale[5], root + 12];
+    return tonicTriad;
+}
+*/
+
+function getNextTriad(scale, scaleIndex, previousScaleIndex, previousTriad){
+
+}
+
+function createTriadProgression(scale, numeralProgression){
+    //triad: array of 3 scale indexes
+    //then they will be converted into keyboard indexes in another function
+    let triadProgression = [];//list of triad arrays
+    //1st inversion triad
+    //root: 3rd of the chord. root: 3g to 4fs
+    const root = scaleToNote(scale, 34, 45);
+    let previousTriad = [3, 5, 1];
+    triadProgression.push(previousTriad);
+    //other 4 triads
+    for(let index = 1; index <= 4; index ++){
+        //use songs to get the next triad (previous chord, current chord)
+        const calculatedTriad = getNextTriad(numeralProgression[index-1], 
+            numeralProgression[index-2]);
+    }  
+
+}
+
+function triadsToKeyboard(){
+
+}
+
