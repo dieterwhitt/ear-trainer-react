e6f26e0cddc8e83e08b2dcd6dfafebf61da34d26
Dieter Whittingham <dieter.whitt@gmail.com>
2024-01-29 11:38:32 -0500

    scaling fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
             <div className='flex-none w-[27%] border-solid rounded-xl border-4
             bg-gradient-to-b from-white to-slage-50 border-indigo-400
             transition ease-out hover:scale-110 duration-300'>
-                <h3 className='text-5xl font-bold py-[4%] bg-indigo-200
+                <h3 className='text-4xl font-bold py-[4%] bg-indigo-200
                 border-solid border-indigo-400 border-b-4 rounded-t-md'>
                     Interval Training
                 </h3>
@@ -31,7 +31,7 @@ const Home = () => {
                     and will range from a perfect unison to a major ninth.
                 </p>
                 <Link to='/intervals'>
-                    <button className='text-4xl font-bold my-[8%] outline rounded-full
+                    <button className='text-3xl font-bold my-[8%] outline rounded-full
                     outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%]
                     bg-indigo-200 hover:bg-indigo-300'>
                         Play Intervals
@@ -41,7 +41,7 @@ const Home = () => {
             <div className='flex-none w-[27%] border-solid rounded-xl border-4
             bg-gradient-to-b from-white to-slage-50 border-indigo-400
             transition ease-out hover:scale-110 duration-300'> 
-                <h3 className='text-5xl font-bold py-[4%] bg-indigo-200
+                <h3 className='text-4xl font-bold py-[4%] bg-indigo-200
                 border-solid border-indigo-400 border-b-4 rounded-t-md'>
                     Chord Identification</h3>
                 <p className='text-2xl leading-[140%] px-[5%] pt-[3%]'>
@@ -50,7 +50,7 @@ const Home = () => {
                     Diminished 7th, Major 7th, Minor 7th, and Augmented Triad.
                 </p>
                 <Link to='/chords' >
-                    <button className='text-4xl font-bold my-[10%] outline rounded-full
+                    <button className='text-3xl font-bold my-[10%] outline rounded-full
                     outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%] 
                     bg-indigo-200 hover:bg-indigo-300'>
                         Play Chord Identification
@@ -60,7 +60,7 @@ const Home = () => {
             <div className='flex-none w-[27%] border-solid rounded-xl border-4
             bg-gradient-to-b from-white to-slage-50 border-indigo-400
             transition ease-out hover:scale-110 duration-300'>
-                <h3 className='text-5xl font-bold py-[4%] bg-indigo-200
+                <h3 className='text-4xl font-bold py-[4%] bg-indigo-200
                 border-solid border-indigo-400 border-b-4 rounded-t-md'>
                     Chord Progressions</h3>
                 <p className='text-2xl leading-[140%] px-[5%] pt-[25%]'>
