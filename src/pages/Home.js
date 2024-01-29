c3f819c38842e2d31eea8961fa207f3c766675d7
Dieter Whittingham <dieter.whitt@gmail.com>
2024-01-28 20:48:55 -0500

    fixed scaling and added final touches

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,21 +17,22 @@ const Home = () => {
             <br/>Also check out the About page!
         </h4>
         <main className='flex flex-row justify-center gap-[4.5%] justify-center 
-        animate-in fade-in slide-in-from-bottom-8 ease-in-out duration-1000'>
+        animate-in fade-in slide-in-from-bottom-[10%] ease-out duration-1000'>
             <div className='flex-none w-[27%] border-solid rounded-xl border-4
             bg-gradient-to-b from-white to-slage-50 border-indigo-400
             transition ease-out hover:scale-110 duration-300'>
-                <h3 className='text-4xl font-bold my-[5%]'>
+                <h3 className='text-5xl font-bold py-[4%] bg-indigo-200
+                border-solid border-indigo-400 border-b-4 rounded-t-md'>
                     Interval Training
                 </h3>
-                <p className='text-2xl leading-[140%] mx-[5%]'>
+                <p className='text-2xl leading-[140%] px-[5%] pt-[3%]'>
                     In interval training, you will be given 10 intervals to 
                     identify. Intervals will be played either broken or simultaneously 
                     and will range from a perfect unison to a major ninth.
                 </p>
                 <Link to='/intervals'>
-                    <button className='text-3xl font-bold my-[10%] outline rounded-full
-                    outline-indigo-400 w-[60%] h-[12%] outline-2 outline-offset-2 
+                    <button className='text-4xl font-bold my-[8%] outline rounded-full
+                    outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%]
                     bg-indigo-200 hover:bg-indigo-300'>
                         Play Intervals
                     </button>
@@ -40,16 +41,17 @@ const Home = () => {
             <div className='flex-none w-[27%] border-solid rounded-xl border-4
             bg-gradient-to-b from-white to-slage-50 border-indigo-400
             transition ease-out hover:scale-110 duration-300'> 
-                <h3 className='text-4xl font-bold my-[5%]'>
+                <h3 className='text-5xl font-bold py-[4%] bg-indigo-200
+                border-solid border-indigo-400 border-b-4 rounded-t-md'>
                     Chord Identification</h3>
-                <p className='text-2xl leading-[140%] mx-[5%]'>
+                <p className='text-2xl leading-[140%] px-[5%] pt-[3%]'>
                     In chord identification, you will be given 10 chords to identify.
                     Possible chords: Major, Minor, Dominant 7th, 
                     Diminished 7th, Major 7th, Minor 7th, and Augmented Triad.
                 </p>
                 <Link to='/chords' >
-                    <button className='text-3xl font-bold my-[10%] outline rounded-full
-                    outline-indigo-400 w-[90%] h-[12%] outline-2 outline-offset-2 
+                    <button className='text-4xl font-bold my-[10%] outline rounded-full
+                    outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%] 
                     bg-indigo-200 hover:bg-indigo-300'>
                         Play Chord Identification
                     </button>
@@ -58,9 +60,10 @@ const Home = () => {
             <div className='flex-none w-[27%] border-solid rounded-xl border-4
             bg-gradient-to-b from-white to-slage-50 border-indigo-400
             transition ease-out hover:scale-110 duration-300'>
-                <h3 className='text-4xl font-bold my-[5%]'>
+                <h3 className='text-5xl font-bold py-[4%] bg-indigo-200
+                border-solid border-indigo-400 border-b-4 rounded-t-md'>
                     Chord Progressions</h3>
-                <p className='text-2xl leading-[140%] mx-[5%] my-[25%]'>
+                <p className='text-2xl leading-[140%] px-[5%] pt-[25%]'>
                     Coming Soon!
                 </p>
                 <Link to='/chordProgressions' >
@@ -68,7 +71,10 @@ const Home = () => {
                 </Link>
             </div>
         </main>
-        <p className='text-xl my-[2%]'>Created by Dieter Whittingham</p>
+        <p className='text-xl my-[2%] 
+        animate-in fade-in slide-in-from-bottom ease-out duration-1000'>
+            Created by Dieter Whittingham
+        </p>
     </div>
     );
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -11,17 +11,18 @@ import { useState } from 'react';
 const Layout = () => {
     return (
     <div className='bg-slate-100 h-screen'>
-        <nav className='flex flex-row items-center
-        bg-indigo-400 font-font1 text-white text-5xl h-[9%]'>
-            <Link className='hover:text-zinc-300 mx-[1.7%]'
+        <nav className='flex flex-row items-center gap-[3%]
+        bg-indigo-300 font-font1 text-white text-5xl h-[9%]
+        px-[1.7%]'>
+            <Link className='hover:text-zinc-300'
             to='/' >Home </Link>
-            <Link className='hover:text-zinc-300 mx-[1.7%]' 
+            <Link className='hover:text-zinc-300' 
             to='/intervals' >Intervals </Link>
-            <Link className='hover:text-zinc-300 mx-[1.7%]'  
+            <Link className='hover:text-zinc-300'  
             to='/chords'>Chords </Link>
-            <Link className='hover:text-zinc-300 mx-[1.7%]' 
+            <Link className='hover:text-zinc-300' 
             to='/chordProgressions'>Chord Progressions </Link>
-            <Link className='hover:text-zinc-300 mx-[1.7%]' 
+            <Link className='hover:text-zinc-300' 
             to='/about'>About</Link>
         </nav>  
 
