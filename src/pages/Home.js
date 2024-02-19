30792f85bcc97a56da45768a88d41ded20bd598f
Dieter Whittingham <dieter.whitt@gmail.com>
2024-02-19 02:20:12 -0500

    fixed tailwind styling inlines

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,64 +6,66 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const homeStyle = 'font-font1 text-center';
+    const titleStyle = 'text-6xl my-[3%] font-bold animate-in fade-in '
+    + 'slide-in-from-top ease-in-out duration-1000';
+    const infoStyle = 'text-3xl my-[2.5%] font-semibold animate-in fade-in '
+    + 'slide-in-from-top ease-in-out duration-1000';
+    const mainStyle = 'flex flex-row justify-center gap-[4.5%] '
+    + 'animate-in fade-in slide-in-from-bottom-[10%] ease-out duration-1000';
+    const boxStyle = 'flex-none w-[27%] border-solid rounded-xl border-4 '
+    + 'bg-gradient-to-b from-white to-slate-50 border-indigo-400 '
+    + 'transition ease-out hover:scale-110 duration-300';
+    const boxHeaderStyle = 'text-4xl font-bold py-[4%] bg-indigo-200 '
+    + 'border-solid border-indigo-400 border-b-4 rounded-t-md';
+    const boxBodyStyle = 'text-2xl leading-[140%] px-[5%] pt-[3%]';
+    const buttonStyle = 'text-3xl font-bold my-[8%] outline rounded-full '
+    + 'outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%] '
+    + 'bg-indigo-200 hover:bg-indigo-300';
+    const footerStyle = 'text-xl pt-[2%] '
+    + 'animate-in fade-in slide-in-from-bottom ease-out duration-1000';
     return (
-    <div className='font-font1 text-center'>
-        <h1 className='text-6xl my-[3%] font-bold animate-in fade-in 
-        slide-in-from-top ease-in-out duration-1000'>Welcome to Ear Trainer!</h1>
-        <h4 className='text-3xl my-[2.5%] font-semibold animate-in fade-in 
-        slide-in-from-top ease-in-out duration-1000'>
+    <div className={homeStyle}>
+        <h1 className={titleStyle}>Welcome to Ear Trainer!</h1>
+        <h4 className={infoStyle}>
             Choose from interval training, chord identification training, or 
             chord progression training!
             <br/>Also check out the About page!
         </h4>
-        <main className='flex flex-row justify-center gap-[4.5%]
-        animate-in fade-in slide-in-from-bottom-[10%] ease-out duration-1000'>
-            <div className='flex-none w-[27%] border-solid rounded-xl border-4
-            bg-gradient-to-b from-white to-slage-50 border-indigo-400
-            transition ease-out hover:scale-110 duration-300'>
-                <h3 className='text-4xl font-bold py-[4%] bg-indigo-200
-                border-solid border-indigo-400 border-b-4 rounded-t-md'>
+        <main className={mainStyle}>
+            <div className={boxStyle}>
+                <h3 className={boxHeaderStyle}>
                     Interval Training
                 </h3>
-                <p className='text-2xl leading-[140%] px-[5%] pt-[3%]'>
+                <p className={boxBodyStyle}>
                     In interval training, you will be given 10 intervals to 
                     identify. Intervals will be played either broken or simultaneously 
                     and will range from a perfect unison to a major ninth.
                 </p>
                 <Link to='/intervals'>
-                    <button className='text-3xl font-bold my-[8%] outline rounded-full
-                    outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%]
-                    bg-indigo-200 hover:bg-indigo-300'>
+                    <button className={buttonStyle}>
                         Play Intervals
                     </button>
                 </Link>
             </div>
-            <div className='flex-none w-[27%] border-solid rounded-xl border-4
-            bg-gradient-to-b from-white to-slage-50 border-indigo-400
-            transition ease-out hover:scale-110 duration-300'> 
-                <h3 className='text-4xl font-bold py-[4%] bg-indigo-200
-                border-solid border-indigo-400 border-b-4 rounded-t-md'>
+            <div className={boxStyle}> 
+                <h3 className={boxHeaderStyle}>
                     Chord Identification</h3>
-                <p className='text-2xl leading-[140%] px-[5%] pt-[3%]'>
+                <p className={boxBodyStyle}>
                     In chord identification, you will be given 10 chords to identify.
                     Possible chords: Major, Minor, Dominant 7th, 
                     Diminished 7th, Major 7th, Minor 7th, and Augmented Triad.
                 </p>
                 <Link to='/chords' >
-                    <button className='text-3xl font-bold my-[10%] outline rounded-full
-                    outline-indigo-400 h-fit outline-2 outline-offset-2 py-[2%] px-[3%] 
-                    bg-indigo-200 hover:bg-indigo-300'>
+                    <button className={buttonStyle}>
                         Play Chord Identification
                     </button>
                 </Link>
             </div>
-            <div className='flex-none w-[27%] border-solid rounded-xl border-4
-            bg-gradient-to-b from-white to-slage-50 border-indigo-400
-            transition ease-out hover:scale-110 duration-300'>
-                <h3 className='text-4xl font-bold py-[4%] bg-indigo-200
-                border-solid border-indigo-400 border-b-4 rounded-t-md'>
+            <div className={boxStyle}>
+                <h3 className={boxHeaderStyle}>
                     Chord Progressions</h3>
-                <p className='text-2xl leading-[140%] px-[5%] pt-[25%]'>
+                <p className={boxBodyStyle}>
                     Coming Soon!
                 </p>
                 <Link to='/chordProgressions' >
@@ -71,8 +73,7 @@ const Home = () => {
                 </Link>
             </div>
         </main>
-        <p className='text-xl my-[2%]
-        animate-in fade-in slide-in-from-bottom ease-out duration-1000'>
+        <p className={footerStyle}>
             Created by Dieter Whittingham
         </p>
     </div>
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,25 +4,25 @@
 
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import { useState } from 'react';
-
 
 
 const Layout = () => {
+    const linkStyle = 'hover:text-zinc-300';
+    const navBarStyle = 'flex flex-row items-center gap-[3%] bg-indigo-300 '
+    + 'font-font1 text-white text-5xl px-[1.7%] py-[0.8%]';
+    const webPageStyle = 'bg-slate-100 min-h-screen flex flex-col';
     return (
-    <div className='bg-slate-100 h-screen resize-none'>
-        <nav className='flex flex-row items-center gap-[3%]
-        bg-indigo-300 font-font1 text-white text-5xl h-[9%]
-        px-[1.7%]'>
-            <Link className='hover:text-zinc-300'
+    <div className={webPageStyle}>
+        <nav className={navBarStyle}>
+            <Link className={linkStyle}
             to='/' >Home </Link>
-            <Link className='hover:text-zinc-300' 
+            <Link className={linkStyle}
             to='/intervals' >Intervals </Link>
-            <Link className='hover:text-zinc-300'  
+            <Link className={linkStyle} 
             to='/chords'>Chords </Link>
-            <Link className='hover:text-zinc-300' 
+            <Link className={linkStyle} 
             to='/chordProgressions'>Chord Progressions </Link>
-            <Link className='hover:text-zinc-300' 
+            <Link className={linkStyle} 
             to='/about'>About</Link>
         </nav>  
 
